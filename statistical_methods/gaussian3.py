4904b7a907661cb0c93ff5dcc08e59fa48ef821f
Nigel Gomes <nigelgomes.ot@gmail.com>
2019-12-06 07:16:17 -0600

    # mean, median added...

---
+++ statistical_methods/gaussian3.py
@@ -0,0 +1,17 @@
+# calculate mean from a sample.
+from numpy.random import seed
+from numpy.random import randn
+from numpy import mean
+from numpy import median
+
+seed(1)
+
+data = 5 * randn(10000) + 50
+
+mean_result = mean(data)
+
+print('Mean: %3f' % mean_result)
+
+median_result = median(data)
+
+print('Median: %3f' % median_result)
