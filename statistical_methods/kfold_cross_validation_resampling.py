e6bf7b7a8f0b1cc4e909c9dc2bc851e6a6b6b862
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-02-14 07:12:25 -0600

    # kfold cross validation resampling added...

---
+++ statistical_methods/kfold_cross_validation_resampling.py
@@ -0,0 +1,10 @@
+#kfold cross validation resampling
+
+from numpy import array
+from sklearn.model_selection import KFold
+
+data = array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+kfold = KFold(3, True, 1)
+
+for train, test in kfold.split(data):
+	print("train: %s, test: %s" % (data[train], data[test]))
