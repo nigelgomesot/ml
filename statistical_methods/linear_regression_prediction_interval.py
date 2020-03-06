edb5e0f19b4dbd1b15b005d0113f1e1c65810487
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-03-06 07:29:09 -0600

    # prediction interval for chosen input added...

--- statistical_methods/linear_regression_prediction_interval.py
+++ statistical_methods/linear_regression_prediction_interval.py
@@ -21,10 +21,24 @@ print("b0:%.3f, b1:%.3f" % (b0, b1))
 
 yhat = b0 + b1 * x
 
-# estimate stdev of hat
+# define new input, output, prediction
+x_in = x[0]
+y_out = y[0]
+yhat_out = yhat[0]
+
+# estimate stdev of yhat
 sum_errors = arraysum((y - yhat)**2)
 stdev = sqrt(1/(len(y) - 2) * sum_errors)
 
+# calculate prediction interval
+interval = 1.96 * stdev
+print("prediction interval: %.3f" % interval)
+
+lower, upper = y_out - interval, y_out + interval
+print("with 95%% likelihood that true value lies between %.3f and %.3f" % (lower, upper))
+print("True value: %.3f" % yhat_out)
+
 pyplot.scatter(x, y)
 pyplot.plot(x, yhat, color='r')
+pyplot.errorbar(x_in, yhat_out, yerr=interval, color='black', fmt='o')
 pyplot.show()
